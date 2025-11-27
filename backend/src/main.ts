e5f7b708d0fc91acd4f8eedd78b8a738180d8090
Robert <ryazdan03@mail.ru>
2025-11-27 15:48:39 +0300

    added auth

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  app.use(cookieParser())
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
+
+  await app.listen(process.env.PORT ?? 5000)
+}
+bootstrap()
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common'
+import { hash } from 'argon2'
+
+import { AuthDto } from '@/auth/dto/auth.dto'
+import { PrismaService } from '@/prisma.service'
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id
+      },
+      include: {
+        stores: true,
+        favorites: true,
+        orders: true
+      }
+    })
+
+    return user
+  }
+
+  async getByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email
+      },
+      include: {
+        stores: true,
+        favorites: true,
+        orders: true
+      }
+    })
+
+    return user
+  }
+
+  async create(dto: AuthDto) {
+    return this.prisma.user.create({
+      data: {
+        name: dto.name,
+        email: dto.email,
+        password: await hash(dto.password)
+      }
+    })
+  }
+}
